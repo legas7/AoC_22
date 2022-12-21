06e58166ee45e1e5136f295492081a027693a3cf
Karol Legaszewski <karol.legaszewski@gmail.com>
2022-12-21 19:39:20 +0100

    day4 pt2 improvements

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -8,13 +8,8 @@ fn main() -> anyhow::Result<()> {
     let overlaps_sum = buffer
         .lines()
         .flat_map(|line| line.split(',').collect_tuple::<(_, _)>())
-        .map(|pair_sections| {
-            (
-                section_to_hashset(pair_sections.0),
-                section_to_hashset(pair_sections.1),
-            )
-        })
-        .map(|(e1, e2)| e1.is_disjoint(&e2))
+        .map(|(s1, s2)| (section_to_hashset(s1), section_to_hashset(s2)))
+        .map(|(hs1, hs2)| hs1.is_disjoint(&hs2))
         .fold(
             0,
             |acc, is_disjoint| if is_disjoint { acc } else { acc + 1 },
@@ -25,12 +20,18 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn section_to_hashset(s: &str) -> HashSet<u32> {
-    s.split("-")
+fn section_to_hashset(section: &str) -> HashSet<u32> {
+    section
+        .split("-")
         .tuples::<(_, _)>()
-        .map(|t| (t.0.parse::<u32>().unwrap(), t.1.parse::<u32>().unwrap()))
-        .map(|pt| {
-            (pt.0..pt.1 + 1)
+        .map(|(beg_str, end_str)| {
+            (
+                beg_str.parse::<u32>().unwrap(),
+                end_str.parse::<u32>().unwrap(),
+            )
+        })
+        .map(|(beg_int, end_int)| {
+            (beg_int..end_int + 1)
                 .collect_vec()
                 .into_iter()
                 .collect::<HashSet<_>>()
