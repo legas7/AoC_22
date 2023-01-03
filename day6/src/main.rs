d989e67b4a0430420d0e2651f090ca4c80c80c70
Karol Legaszewski <karol.legaszewski@gmail.com>
2023-01-03 19:20:33 +0100

    day6 pt2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,18 +1,17 @@
 use itertools::Itertools;
 use std::{fs::File, io::Read};
-use tuple_conv::RepeatedTuple;
 
 fn main() -> anyhow::Result<()> {
     let mut buffer = String::new();
     _ = File::open("inputs/day6_input.txt")?.read_to_string(&mut buffer)?;
 
-    let marker_offset = buffer
-        .chars()
-        .tuple_windows::<(_, _, _, _)>()
+    let marker_offset = &buffer
+        .as_bytes()
+        .windows(14)
         .enumerate()
         .flat_map(|(i, t)| {
             if t.to_vec().iter().all_unique() {
-                Some(i + 4)
+                Some(i + 14)
             } else {
                 None
             }
